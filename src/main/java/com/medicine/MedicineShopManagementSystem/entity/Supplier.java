1c06fabe81bbef0e543b70456b20efc94aa8a9bf
mahfujur-shohan <mahfujurrahmancityu@gmail.com>
2024-10-03 01:17:35 +0600

    added supplier entity details]

--- src/main/java/com/medicine/MedicineShopManagementSystem/entity/Supplier.java
+++ src/main/java/com/medicine/MedicineShopManagementSystem/entity/Supplier.java
@@ -1,7 +1,90 @@
 package com.medicine.MedicineShopManagementSystem.entity;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "Supplier")
 public class Supplier {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int supplierId;
 
+    private String supplierName;
 
+    private String supplierEmail;
+
+    private String supplierContact;
+
+    private String supplierAddress;
+
+    private String companyName;
+
+    private String contactPerson;
+
+    private String drugsSupplied;
+
+    public int getSupplierId() {
+        return supplierId;
+    }
+
+    public void setSupplierId(int supplierId) {
+        this.supplierId = supplierId;
+    }
+
+    public String getSupplierName() {
+        return supplierName;
+    }
+
+    public void setSupplierName(String supplierName) {
+        this.supplierName = supplierName;
+    }
+
+    public String getSupplierEmail() {
+        return supplierEmail;
+    }
+
+    public void setSupplierEmail(String supplierEmail) {
+        this.supplierEmail = supplierEmail;
+    }
+
+    public String getSupplierContact() {
+        return supplierContact;
+    }
+
+    public void setSupplierContact(String supplierContact) {
+        this.supplierContact = supplierContact;
+    }
+
+    public String getSupplierAddress() {
+        return supplierAddress;
+    }
+
+    public void setSupplierAddress(String supplierAddress) {
+        this.supplierAddress = supplierAddress;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public String getContactPerson() {
+        return contactPerson;
+    }
+
+    public void setContactPerson(String contactPerson) {
+        this.contactPerson = contactPerson;
+    }
+
+    public String getDrugsSupplied() {
+        return drugsSupplied;
+    }
+
+    public void setDrugsSupplied(String drugsSupplied) {
+        this.drugsSupplied = drugsSupplied;
+    }
 }
