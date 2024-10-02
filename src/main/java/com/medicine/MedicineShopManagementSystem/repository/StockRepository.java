476ce08f30a31e305e88320c4dad2427f3ec3fda
mahfujur-shohan <mahfujurrahmancityu@gmail.com>
2024-10-03 01:30:46 +0600

    added both stock and supplier repository using jparepositoryimplementation

---
+++ src/main/java/com/medicine/MedicineShopManagementSystem/repository/StockRepository.java
@@ -0,0 +1,9 @@
+package com.medicine.MedicineShopManagementSystem.repository;
+
+import com.medicine.MedicineShopManagementSystem.entity.Stock;
+import org.springframework.data.jpa.repository.support.JpaRepositoryImplementation;
+
+public interface StockRepository extends JpaRepositoryImplementation<Stock, Integer> {
+
+
+}
---
+++ src/main/java/com/medicine/MedicineShopManagementSystem/repository/SupplierRepository.java
@@ -0,0 +1,9 @@
+package com.medicine.MedicineShopManagementSystem.repository;
+
+import com.medicine.MedicineShopManagementSystem.entity.Supplier;
+import org.springframework.data.jpa.repository.support.JpaRepositoryImplementation;
+
+public interface SupplierRepository extends JpaRepositoryImplementation<Supplier, Integer> {
+
+
+}
