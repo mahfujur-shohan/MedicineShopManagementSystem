b511cd9d5d82cb7c5cd01132f17eec8b8bd2b3c9
mahfujur-shohan <mahfujurrahmancityu@gmail.com>
2024-10-03 01:26:23 +0600

    added stock and supplier data transfer object

---
+++ src/main/java/com/medicine/MedicineShopManagementSystem/DTO/SupplierDTO.java
@@ -0,0 +1,74 @@
+package com.medicine.MedicineShopManagementSystem.DTO;
+
+public class SupplierDTO {
+
+    private String supplierName;
+
+    private String supplierEmail;
+
+    private String supplierContact;
+
+    private String supplierAddress;
+
+    private String companyName;
+
+    private String contactPerson;
+
+    private String drugsSupplied;
+
+    public String getSupplierName() {
+        return supplierName;
+    }
+
+    public void setSupplierName(String supplierName) {
+        this.supplierName = supplierName;
+    }
+
+    public String getSupplierEmail() {
+        return supplierEmail;
+    }
+
+    public void setSupplierEmail(String supplierEmail) {
+        this.supplierEmail = supplierEmail;
+    }
+
+    public String getSupplierContact() {
+        return supplierContact;
+    }
+
+    public void setSupplierContact(String supplierContact) {
+        this.supplierContact = supplierContact;
+    }
+
+    public String getSupplierAddress() {
+        return supplierAddress;
+    }
+
+    public void setSupplierAddress(String supplierAddress) {
+        this.supplierAddress = supplierAddress;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public String getContactPerson() {
+        return contactPerson;
+    }
+
+    public void setContactPerson(String contactPerson) {
+        this.contactPerson = contactPerson;
+    }
+
+    public String getDrugsSupplied() {
+        return drugsSupplied;
+    }
+
+    public void setDrugsSupplied(String drugsSupplied) {
+        this.drugsSupplied = drugsSupplied;
+    }
+}
