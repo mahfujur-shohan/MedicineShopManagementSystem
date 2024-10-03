2709848fcb2f0fffb742dc8edbb3a4cd4f91e1ac
mahfujur-shohan <mahfujurrahmancityu@gmail.com>
2024-10-03 15:46:05 +0600

    added stock service implementation

---
+++ src/main/java/com/medicine/MedicineShopManagementSystem/services/StockService.java
@@ -0,0 +1,17 @@
+package com.medicine.MedicineShopManagementSystem.services;
+
+import com.medicine.MedicineShopManagementSystem.entity.Stock;
+
+import java.util.List;
+
+public interface StockService {
+
+    List<Stock> findAll();
+
+    Stock findById(int id);
+
+    Stock save(Stock stock);
+
+    void deleteById(int id);
+
+}
---
+++ src/main/java/com/medicine/MedicineShopManagementSystem/services/StockServiceImpl.java
@@ -0,0 +1,52 @@
+package com.medicine.MedicineShopManagementSystem.services;
+
+import com.medicine.MedicineShopManagementSystem.entity.Stock;
+import com.medicine.MedicineShopManagementSystem.repository.StockRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class StockServiceImpl implements StockService {
+
+    private StockRepository stockRepository;
+
+    @Autowired
+    public StockServiceImpl(StockRepository theStockRepository) {
+        this.stockRepository = theStockRepository;
+    }
+
+
+    @Override
+    public List<Stock> findAll() {
+        return stockRepository.findAll();
+    }
+
+    @Override
+    public Stock findById(int id) {
+        Optional<Stock> stock = stockRepository.findById(id);
+
+        Stock stockObj = null;
+
+        if (stock.isPresent()) {
+            stockObj = stock.get();
+        }
+        else {
+            throw new RuntimeException("Stock not found with id " + id);
+        }
+
+        return stockObj;
+    }
+
+    @Override
+    public Stock save(Stock stock) {
+        return stockRepository.save(stock);
+    }
+
+    @Override
+    public void deleteById(int id) {
+        stockRepository.deleteById(id);
+    }
+}
