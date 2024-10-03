22e6e59d8c20ccc8024b7a5a30af6c254c66a46c
mahfujur-shohan <mahfujurrahmancityu@gmail.com>
2024-10-03 17:15:11 +0600

    stock controller added]

---
+++ src/main/java/com/medicine/MedicineShopManagementSystem/controllers/StockController.java
@@ -0,0 +1,65 @@
+package com.medicine.MedicineShopManagementSystem.controllers;
+
+import com.medicine.MedicineShopManagementSystem.entity.Stock;
+import com.medicine.MedicineShopManagementSystem.services.StockService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class StockController {
+
+    private StockService stockService;
+
+    public StockController(StockService theStockService) {
+        this.stockService = theStockService;
+    }
+
+    @GetMapping("/stocks")
+    public List<Stock> findAll() {
+        return stockService.findAll();
+    }
+
+    @GetMapping("/stocks/{stockId}")
+    public Stock findById(@PathVariable int stockId) {
+        Stock stock = stockService.findById(stockId);
+
+        if(stock == null) {
+            throw new RuntimeException("Stock not found with id " + stockId);
+        }
+
+        return stock;
+    }
+
+    @PostMapping("/stocks")
+    public Stock addStock(@RequestBody Stock stock) {
+
+        stock.setDrugId(0);
+
+        Stock savedStock = stockService.save(stock);
+
+        return savedStock;
+    }
+
+    @PutMapping("/stocks")
+    public Stock updateStock(@RequestBody Stock stock) {
+        Stock savedStock = stockService.save(stock);
+
+        return savedStock;
+    }
+
+    @DeleteMapping("/stocks/{stockId}")
+    public String deleteById(@PathVariable int stockId) {
+        Stock stock = stockService.findById(stockId);
+
+        if(stock == null) {
+            throw new RuntimeException("Stock not found with id " + stockId);
+        }
+
+        stockService.deleteById(stockId);
+
+        return "Stock with id " + stockId + " deleted successfully";
+    }
+
+}
